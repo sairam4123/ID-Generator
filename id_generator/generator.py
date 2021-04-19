75e16ae86f47b40797c670073f21e7b563d8621b
sairam4123 <sairamkumar2022@gmail.com>
2021-04-19 17:20:04 +0530

    Update comments, implement serial workers, update example.

--- id_generator/generator.py
+++ id_generator/generator.py
@@ -1,5 +1,6 @@
 import random
 import time
+from typing import List
 
 
 class IDProcessWorker:
@@ -37,7 +38,7 @@ class IDProcessWorker:
 
 
 class IDGenerator:
-    def __init__(self, epoch, process_id_bits=5, worker_id_bits=5, increment_bits=5):
+    def __init__(self, epoch: int, process_id_bits=5, worker_id_bits=5, increment_bits=12, random_worker=True):
         # ID bits
         self.process_id_bits = process_id_bits
         self.worker_id_bits = worker_id_bits
@@ -53,8 +54,11 @@ class IDGenerator:
         self.timestamp_left_shift = self.process_id_bits + self.process_id_left_shift
         self.epoch = epoch
 
-        self.workers = []
-        self.workers_full = []
+        self.workers: List['IDProcessWorker'] = []
+        self.workers_full: List['IDProcessWorker'] = []
+        self.random_worker = random_worker
+        if not self.random_worker:
+            self.serial_worker_index = 0
 
     def create_process_and_worker(self, worker_id, process_id, increment=0) -> 'IDProcessWorker':
         worker = IDProcessWorker(worker_id, process_id, self.epoch, self, increment)
@@ -63,11 +67,16 @@ class IDGenerator:
         return worker
 
     def create_id(self) -> int:
-        random.shuffle(self.workers)
-        random_worker = self.workers.pop(0)
-        if not self.workers:
-            self.workers = self.workers_full.copy()
-        return random_worker.get_next_id()
+        if self.random_worker:
+            random.shuffle(self.workers)
+            random_worker = self.workers.pop(0)
+            if not self.workers:
+                self.workers = self.workers_full.copy()
+            return random_worker.get_next_id()
+        else:
+            serial_worker = self.workers_full[self.serial_worker_index]
+            self.serial_worker_index %= len(self.workers_full)
+            serial_worker.get_next_id()
 
     def get_worker(self, worker_id, process_id) -> 'IDProcessWorker':
         for worker in self.workers_full:
@@ -77,24 +86,26 @@ class IDGenerator:
 
 if __name__ == '__main__':
     import datetime
-    ID_EPOCH = 1577836800
 
-    id_gen_1 = IDGenerator(ID_EPOCH)
+    EPOCH = 1577836800  # 1st Jan 2020
 
+    id_gen_1 = IDGenerator(EPOCH)  # Make sure not to pass EPOCH as milliseconds.
+
+    # Create some workers. This snippet creates 40 workers, creating 4 processes containing 10 workers in each process.
     for j in range(4):
         for i in range(10):
-            id_gen_1.create_process_and_worker(i, j)
+            id_gen_1.create_process_and_worker(i, j)  # You must create a worker to create IDs.
 
-    id_array = []
-    for j in range(100000):
-        created_id = id_gen_1.create_id()
-        # print(created_id)
-        id_array.append(created_id)
-
-    print(len(set(id_array)), len(id_array))
+    # Create ID
     starting_id = id_gen_1.create_id()
-    print(starting_id)
-    print(datetime.datetime.utcfromtimestamp(int(((starting_id >> 22) + ID_EPOCH * 1000.0) / 1000.0)) + datetime.timedelta(hours=5, minutes=30))
-    print((starting_id & 0x3E0000) >> 17)
-    print((starting_id & 0x1F000) >> 12)
-    print((starting_id & 0xFFF))
+    print("Recent ID:", starting_id)
+    # Get UTC Time
+    utc_time_stamp = int(((starting_id >> 22) + (EPOCH * 1000.0)) / 1000.0)  # Divide by 1000.0 to get seconds.
+    print("UTC Timestamp:", utc_time_stamp)
+    #                                                    ^ Multiply by 1000.0 to get milliseconds, as the timestamp is in millisecond precision.
+    utc_time = datetime.datetime.utcfromtimestamp(utc_time_stamp).replace(tzinfo=datetime.timezone.utc)
+    print("UTC Time:", utc_time)
+    print("Local Time:", utc_time.astimezone(tz=None))
+    print("Process ID:", (starting_id & 0x3E0000) >> 17)
+    print("Worker ID:", (starting_id & 0x1F000) >> 12)
+    print("Increment:", (starting_id & 0xFFF))
